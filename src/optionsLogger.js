0c1e6b775ef79765c2d619cfec2897ebf1f6d958
Adam Nowotny <adam.nowotny@gmail.com>
2012-10-10 02:06:13 +1100

    alert for unhandled errors

--- src/optionsLogger.js
+++ src/optionsLogger.js
@@ -18,7 +18,9 @@ define([
 		}
 
 		window.onerror = function (message, url, line) {
-			window.console.error(interpolate('Unhandled error. message=[{{0}}], url=[{{1}}], line=[{{2}}]', [message, url, line]));
+			var displayMessage = interpolate('Unhandled error. message=[{{0}}], url=[{{1}}], line=[{{2}}]', [message, url, line]);
+			window.console.error(displayMessage);
+			alert(displayMessage);
 			return false; // don't suppress default handling
 		};
 	}
