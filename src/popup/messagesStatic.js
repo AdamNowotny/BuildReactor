f1094a66c623764dcbd2b5dcead9a6cf842207be
Adam Nowotny <adam.nowotny@gmail.com>
2013-02-13 21:07:19 +1100

    show building status for testing

--- src/popup/messagesStatic.js
+++ src/popup/messagesStatic.js
@@ -2,7 +2,7 @@ define(['rx', 'rx.time'], function (Rx) {
 	
 	'use strict';
 
-	var activeProjects = JSON.parse('[{"name":"OpenMRS","items":[{"name":"Application Release Test","group":"Functional Tests","isBroken":false,"url":null,"isBuilding":false},{"name":"Performance Test","group":"Functional Tests","isBroken":true,"url":"http://ci.openmrs.org/browse/FUNC-PERF-4","isBuilding":false}]}]');
+	var activeProjects = JSON.parse('[{"name":"OpenMRS","items":[{"name":"Application Release Test","group":"Functional Tests","isBroken":false,"url":null,"isBuilding":true},{"name":"Performance Test","group":"Functional Tests","isBroken":true,"url":"http://ci.openmrs.org/browse/FUNC-PERF-4","isBuilding":true}]}]');
 
 	return {
 		current: Rx.Observable.returnValue(activeProjects)
