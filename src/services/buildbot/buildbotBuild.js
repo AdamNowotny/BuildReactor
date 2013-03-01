11fe895baf2f7efe4296ca9553a6e11066ba520d
James Rhodes <jrhodes@redpointsoftware.com.au>
2013-03-02 01:55:52 +1100

    Added support for BuildBot CI.

---
+++ src/services/buildbot/buildbotBuild.js
@@ -0,0 +1,80 @@
+define([
+	'services/build',
+	'services/buildbot/buildbotRequest',
+	'signals',
+	'jquery'
+], function (Build, request, Signal, $) {
+	'use strict';
+
+	var BuildBotBuild = function (id, settings) {
+		$.extend(this, new Build(id, settings));
+		this.name = id;
+		this.projectName = null;
+	};
+
+	function activateEvents(build, active) {
+		build.on.broken.active = active;
+		build.on.fixed.active = active;
+		build.on.started.active = active;
+		build.on.finished.active = active;
+	}
+
+	BuildBotBuild.prototype.update = function () {
+
+		var builderResponseHandler = function (result) {
+            that.webUrl = that.settings.url + "builders/" + that.id;
+            that.isDisabled = false;
+			activateEvents(that, !that.isDisabled);
+			var isBuilding = result.response.state === "building";
+			if (!that.isRunning && isBuilding) {
+				that.isRunning = true;
+				that.on.started.dispatch(that);
+			} else if (that.isRunning && !isBuilding) {
+				that.isRunning = false;
+				that.on.finished.dispatch(that);
+			}
+			request.lastCompletedBuild(that.settings, that.id).addOnce(function (result) {
+				updateResult(that, result);
+				completed.dispatch();
+			});
+		};
+
+		var updateResult = function (build, result) {
+			if (build.isBroken && result.response.text.indexOf('successful') !== -1) {
+				build.isBroken = false;
+				build.on.fixed.dispatch(that);
+			}
+			if (!build.isBroken && result.response.text.indexOf('failed') !== -1) {
+				build.isBroken = true;
+				build.on.broken.dispatch(that);
+			}
+		};
+
+		var buildRunningResponseHandler = function (result) {
+			if (result.error) {
+				that.on.errorThrown.dispatch(that);
+			} else if (!that.isRunning && result.response.running) {
+				that.isRunning = true;
+				that.on.started.dispatch(that);
+			} else if (that.isRunning && !result.response.running) {
+				that.isRunning = false;
+				that.on.finished.dispatch(that);
+			}
+		};
+
+		var completed = new Signal();
+		completed.memorize = true;
+		var that = this;
+		request.builder(this.settings, this.id).addOnce(function (result) {
+			if (result.error) {
+				that.on.errorThrown.dispatch(that);
+				completed.dispatch();
+			} else {
+				builderResponseHandler(result);
+			}
+		});
+		return completed;
+	};
+
+	return BuildBotBuild;
+});
---
+++ src/services/buildbot/buildbotRequest.js
@@ -0,0 +1,73 @@
+define([
+		'signals',
+		'main/ajaxRequest',
+		'common/joinUrl'
+	], function (signals, AjaxRequest, joinUrl) {
+
+		'use strict';
+
+		var send = function (ajaxSettings) {
+			var completed = new signals.Signal();
+			var request = new AjaxRequest(ajaxSettings);
+			completed.memorize = true;
+			request.on.responseReceived.addOnce(function (response) {
+				completed.dispatch({ response: response });
+			}, this);
+			request.on.errorReceived.addOnce(function (ajaxError) {
+				completed.dispatch({ error: ajaxError });
+			}, this);
+			request.send();
+			return completed;
+		};
+
+		function projects(settings) {
+			if (!(settings && settings.url && settings.url !== '')) {
+				throw {
+					name: 'ArgumentInvalid',
+					message: 'settings.url not set'
+				};
+			}
+			var ajaxSettings = {
+				url: joinUrl(settings.url, 'json/builders'),
+				username: settings.username,
+				password: settings.password
+			};
+			return send(ajaxSettings);
+		}
+
+		var builder = function (settings, id) {
+			if (!(settings && settings.url && settings.url !== '')) {
+				throw {
+					name: 'ArgumentInvalid',
+					message: 'settings.url not set'
+				};
+			}
+			var ajaxSettings = {
+				url: joinUrl(settings.url, 'json/builders/' + id + '/builds'),
+				username: settings.username,
+				password: settings.password
+			};
+			return send(ajaxSettings);
+		};
+
+		var lastCompletedBuild = function (settings, id) {
+			if (!(settings && settings.url && settings.url !== '')) {
+				throw {
+					name: 'ArgumentInvalid',
+					message: 'settings.url not set'
+				};
+			}
+			var ajaxSettings = {
+				url: joinUrl(settings.url, 'json/builders/' + id + '/builds/-1'),
+				username: settings.username,
+				password: settings.password
+			};
+			return send(ajaxSettings);
+		};
+
+		return {
+			projects: projects,
+			builder: builder,
+			lastCompletedBuild: lastCompletedBuild
+		};
+	});
