898b3248fee336f9518a25c706cd30aec79a9db1
Adam Nowotny <adam.nowotny@gmail.com>
2013-09-06 01:45:29 +1000

    dashboard: fix links, wrong scope reference

--- src/dashboard/main.js
+++ src/dashboard/main.js
@@ -5,15 +5,19 @@ define([
 	'use strict';
 
 	return app.config(function ($routeProvider, $locationProvider) {
-			$routeProvider
-			.when('/', {
-				templateUrl: 'src/dashboard/view.html',
-				controller: 'DashboardCtrl'
-			})
-			.otherwise({
-				redirectTo: '/'
-			});
+		$routeProvider
+		.when('/', {
+			templateUrl: 'src/dashboard/view.html',
+			controller: 'DashboardCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
 
-			$locationProvider.html5Mode(false);
-		}); 
+		$locationProvider.html5Mode(false);
+	}).config([
+		'$compileProvider', function ($compileProvider)	{
+			$compileProvider.urlSanitizationWhitelist(/^\s*(https?|chrome-extension):/);
+		}
+	]);
 });
