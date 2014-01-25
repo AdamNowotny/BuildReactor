23706a7df1d15a421ddf3ef15b6bbe7f028a0ce3
Adam Nowotny <adam.nowotny@gmail.com>
2014-01-25 11:08:23 +1100

    moved show and save buttons out of dynamic form

--- src/settings/serviceSettingsCtrl.js
+++ src/settings/serviceSettingsCtrl.js
@@ -7,5 +7,14 @@ define([
 
 	app.controller('ServiceSettingsCtrl', function ($scope, $routeParams) {
 
+		$scope.show = function () {
+			var settings;
+			$scope.$emit('dynamicForm.showClicked', settings);
+		};
+
+		$scope.save = function () {
+			$scope.$emit('dynamicForm.saveClicked');
+		};
+
 	});
 });
