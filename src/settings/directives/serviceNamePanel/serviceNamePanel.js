909ad8e6eedfba300b79fb724fef9115b7812501
Adam Nowotny <adam.nowotny@gmail.com>
2015-03-31 20:17:31 +1100

    settings: don't allow to add service with existing name

--- src/settings/directives/serviceNamePanel/serviceNamePanel.js
+++ src/settings/directives/serviceNamePanel/serviceNamePanel.js
@@ -1,6 +1,7 @@
 define([
-	'settings/app'
-], function (app) {
+	'settings/app',
+	'common-ui/core'
+], function (app, core) {
 	'use strict';
 
 	app.directive('serviceNamePanel', function () {
@@ -11,9 +12,23 @@ define([
 			templateUrl: 'src/settings/directives/serviceNamePanel/serviceNamePanel.html',
 			controller: function ($scope, $element, $attrs, $transclude) {
 				$scope.serviceName = '';
+				
 				$scope.add = function () {
 					$scope.$emit('serviceNamePanel.added', $scope.serviceName);
 				};
+
+				$scope.$watch('serviceName', function (name) {
+					$scope.exists = $scope.services ? $scope.services.filter(function (service) {
+						return service.name === name;
+					}).length > 0 : false;
+				});
+
+				core.configurations.subscribe(function (configs) {
+					$scope.$evalAsync(function () {
+						$scope.services = configs;
+					});
+				});
+
 			}
 		};
 	});
