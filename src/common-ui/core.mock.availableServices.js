2b411fd674e357a572f1e25f87da0e1b45dffc32
Adam Nowotny <adam.nowotny@gmail.com>
2015-10-30 21:18:30 +1100

    update core.mock.availableServices

--- src/common-ui/core.mock.availableServices.js
+++ src/common-ui/core.mock.availableServices.js
@@ -4,108 +4,128 @@ define(function () {
 
 	return [
 		{
-			typeName: "Atlassian Bamboo",
-			baseUrl: "bamboo",
-			icon: "bamboo/icon.png",
-			logo: "bamboo/logo.png",
-			projects: [],
-			url: "",
-			urlHint: "https://ci.openmrs.org/",
-			username: "",
-			password: "",
-			updateInterval: 60
+			typeName: 'Atlassian Bamboo',
+			baseUrl: 'bamboo',
+			urlHint: 'URL, e.g. http://ci.openmrs.org/',
+			urlHelp: 'For Bamboo OnDemand use https://[your_account].atlassian.net/builds',
+			defaultConfig: {
+				baseUrl: 'bamboo',
+				name: '',
+				projects: [],
+				url: '',
+				username: '',
+				password: '',
+				updateInterval: 60
+			}
 		},
 		{
 			typeName: "CCTray Generic",
 			baseUrl: "cctray",
-			icon: "cctray/icon.png",
-			logo: "cctray/logo.png",
-			projects: [],
-			url: "",
 			urlHint: "http://cruisecontrol.instance.com/cctray.xml",
-			username: "",
-			password: "",
-			updateInterval: 60
+			defaultConfig: {
+				baseUrl: 'cctray',
+				name: '',
+				projects: [],
+				url: '',
+				username: '',
+				password: '',
+				updateInterval: 60
+			}
 		},
 		{
 			typeName: "CruiseControl",
 			baseUrl: "cruisecontrol",
-			icon: "cruisecontrol/icon.png",
-			logo: "cruisecontrol/logo.png",
-			projects: [],
-			url: "",
 			urlHint: "http://cruisecontrol.instance.com/",
-			username: "",
-			password: "",
-			updateInterval: 60
+			defaultConfig: {
+				baseUrl: 'cruisecontrol',
+				name: '',
+				projects: [],
+				url: '',
+				username: '',
+				password: '',
+				updateInterval: 60
+			}
 		},
 		{
 			typeName: "CruiseControl.NET",
 			baseUrl: "cruisecontrol.net",
-			icon: "cruisecontrol.net/icon.png",
-			logo: "cruisecontrol.net/logo.png",
-			projects: [],
-			url: "",
 			urlHint: "http://build.nauck-it.de/",
-			username: "",
-			password: "",
-			updateInterval: 60
+			defaultConfig: {
+				baseUrl: 'cruisecontrol.net',
+				name: '',
+				projects: [],
+				url: '',
+				username: '',
+				password: '',
+				updateInterval: 60
+			}
 		},
 		{
 			typeName: "CruiseControl.rb",
 			baseUrl: "cruisecontrol.rb",
-			icon: "cruisecontrol.rb/icon.png",
-			logo: "cruisecontrol.rb/logo.png",
-			projects: [],
-			url: "",
 			urlHint: "http://cruisecontrolrb.thoughtworks.com/",
-			username: "",
-			password: "",
-			updateInterval: 60
+			defaultConfig: {
+				baseUrl: 'cruisecontrol.rb',
+				name: '',
+				projects: [],
+				url: '',
+				username: '',
+				password: '',
+				updateInterval: 60
+			}
 		},
 		{
 			typeName: "ThoughtWorks GO",
 			baseUrl: "go",
-			icon: "go/icon.png",
-			logo: "go/logo.png",
-			projects: [],
-			url: "",
 			urlHint: "http://example-go.thoughtworks.com/",
-			username: "",
-			password: "",
-			updateInterval: 60
+			defaultConfig: {
+				baseUrl: 'go',
+				name: '',
+				projects: [],
+				url: '',
+				username: '',
+				password: '',
+				updateInterval: 60
+			}
 		},
 		{
 			typeName: "Jenkins",
 			baseUrl: "jenkins",
-			icon: "jenkins/icon.png",
-			logo: "jenkins/logo.png",
-			projects: [],
-			url: "",
 			urlHint: "http://ci.jenkins-ci.org/",
-			username: "",
-			password: "",
-			updateInterval: 60
+			defaultConfig: {
+				baseUrl: 'jenkins',
+				name: '',
+				projects: [],
+				url: '',
+				username: '',
+				password: '',
+				updateInterval: 60
+			}
 		},
 		{
 			typeName: "TeamCity",
 			baseUrl: "teamcity",
-			icon: "teamcity/icon.png",
-			logo: "teamcity/logo.png",
-			projects: [],
-			url: "",
 			urlHint: "http://teamcity.jetbrains.com/",
-			username: "",
-			password: "",
-			updateInterval: 60
+			defaultConfig: {
+				baseUrl: 'teamcity',
+				name: '',
+				projects: [],
+				url: '',
+				username: '',
+				password: '',
+				updateInterval: 60
+			}
 		},
 		{
 			typeName: "Travis",
 			baseUrl: "travis",
-			icon: "travis/icon.png",
-			logo: "travis/logo.png",
-			projects: [],
-			username: ""
+			defaultConfig: {
+				baseUrl: 'travis',
+				name: '',
+				projects: [],
+				username: '',
+				updateInterval: 60
+			}
 		}
 	];
 });
