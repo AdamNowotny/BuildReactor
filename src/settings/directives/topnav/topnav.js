284fa452cad868548cd3f105cb459d43cef83591
Adam Nowotny <adam.nowotny@gmail.com>
2017-05-01 18:05:20 +1000

    fix hiding topnav when switching to other pages

--- src/settings/directives/topnav/topnav.js
+++ src/settings/directives/topnav/topnav.js
@@ -9,66 +9,55 @@ import removeTemplate from 'settings/directives/topnav/removeModal.html';
 import renameTemplate from 'settings/directives/topnav/renameModal.html';
 import template from 'settings/directives/topnav/topnav.html';
 
-export default app.directive('topnav', function($uibModal, $location) {
-	return {
-		scope: {
-			service: '=currentService',
-			showActions: '='
-		},
-		templateUrl: template,
-		controller: function($scope, $element, $attrs, $transclude) {
+export default app.directive('topnav', ($uibModal, $location) => ({
+	scope: {
+		service: '=currentService',
+		showActions: '='
+	},
+	templateUrl: template,
+	controller($scope, $element, $attrs, $transclude) {
 
-			$scope.$watch('service', function(selectedService) {
-				if (selectedService) {
-					$scope.isActive = $scope.showActions;
-					$scope.isEnabled = !selectedService.disabled;
-				} else {
-					$scope.isActive = false;
+		$scope.$watch('service', function(selectedService) {
+			$scope.isEnabled = selectedService && !selectedService.disabled;
+		});
+
+		$scope.$on('onOffSwitch.change', (event, isEnabled) => {
+			if (!$scope.service) {
+				return;
+			}
+			if (isEnabled) {
+				core.enableService($scope.service.name);
+			} else {
+				core.disableService($scope.service.name);
+			}
+		});
+
+		$scope.remove = function() {
+			$uibModal.open({
+				templateUrl: removeTemplate,
+				controller: 'RemoveModalCtrl',
+				scope: $scope,
+				resolve: {
+					serviceName: () => $scope.service.name
 				}
+			}).result.then((serviceName) => {
+				core.removeService(serviceName);
+				$location.path('/new/').replace();
 			});
+		};
 
-			$scope.$on('onOffSwitch.change', function(event, isEnabled) {
-				if (!$scope.service) {
-					return;
+		$scope.rename = function() {
+			$uibModal.open({
+				templateUrl: renameTemplate,
+				controller: 'RenameModalCtrl',
+				resolve: {
+					serviceName: () => $scope.service.name
 				}
-				if (isEnabled) {
-					core.enableService($scope.service.name);
-				} else {
-					core.disableService($scope.service.name);
-				}
+			}).result.then((serviceName) => {
+				core.renameService($scope.service.name, serviceName);
+				$location.path(`/service/${serviceName}`).replace();
 			});
+		};
 
-			$scope.remove = function() {
-				$uibModal.open({
-					templateUrl: removeTemplate,
-					controller: 'RemoveModalCtrl',
-					scope: $scope,
-					resolve: {
-						serviceName: function() {
-							return $scope.service.name;
-						}
-					}
-				}).result.then(function(serviceName) {
-					core.removeService(serviceName);
-					$location.path('/new/').replace();
-				});
-			};
-
-			$scope.rename = function() {
-				$uibModal.open({
-					templateUrl: renameTemplate,
-					controller: 'RenameModalCtrl',
-					resolve: {
-						serviceName: function() {
-							return $scope.service.name;
-						}
-					}
-				}).result.then(function(serviceName) {
-					core.renameService($scope.service.name, serviceName);
-					$location.path('/service/' + serviceName).replace();
-				});
-			};
-
-		}
-	};
-});
+	}
+}));
