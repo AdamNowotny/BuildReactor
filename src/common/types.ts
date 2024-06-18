1abc5892889648bf3d630306ee953a06d7bb08b8
Adam Nowotny <adam.nowotny@protonmail.com>
2024-06-17 23:34:41 -0500

    TS types for data returned from services

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,3 +1,70 @@
+import Rx from 'rxjs';
+
+export interface CIServiceSettings {
+    baseUrl: string;
+    name: string;
+    token?: string;
+    branch?: string;
+    url?: string;
+    apiUrl?: string;
+    webUrl?: string;
+    username?: string;
+    password?: string;
+    projects: string[];
+    updateInterval?: number;
+}
+
+interface CIServiceInfoField {
+    type: string;
+    name?: string;
+    help?: string;
+    header?: string;
+    config?: string;
+}
+
+interface CIServiceInfo {
+    typeName: string;
+    baseUrl: string;
+    icon: string;
+    logo: string;
+    fields: CIServiceInfoField[];
+    defaultConfig: CIServiceSettings
+}
+
+export interface CIPipeline {
+    id: number;
+    name: string;
+    group?: boolean;
+    isDisabled?: boolean;
+}
+
+interface CIBuildTag {
+    name: string;
+    type?: string;
+    description?: string;
+}
+
+interface CIBuildChange {
+    name: string;
+    message?: string;
+}
+
+export interface CIBuild {
+    id: string,
+    name: string,
+    group: string | null,
+    isBroken?: boolean,
+    isRunning?: boolean,
+    isWaiting?: boolean,
+    isDisabled?: boolean,
+    error?: { message: string },
+    webUrl?: string,
+    tags?: CIBuildTag[],
+    changes?: CIBuildChange[],
+}
+
 export interface CIService {
-    getInfo: () => { baseUrl: string; };
+    getInfo(): CIServiceInfo;
+    getAll(settings: CIServiceSettings): Rx.Observable<CIPipeline>;
+    getLatest(settings: CIServiceSettings): Rx.Observable<CIBuild>;
 }
