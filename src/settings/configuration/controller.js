08788d478ab196fca1ea4aa987dbc1f7c744574a
Adam Nowotny <adam.nowotny@protonmail.com>
2024-07-27 23:38:57 +0200

    settings: hide token field value, similar to password

--- src/settings/configuration/controller.js
+++ src/settings/configuration/controller.js
@@ -4,40 +4,45 @@ import angular from 'angular';
 import app from 'settings/app';
 import core from 'common/core';
 
-export default app.controller('ConfigurationCtrl', function($scope, $http) {
+export default app.controller('ConfigurationCtrl', function ($scope, $http) {
     $scope.includePasswords = false;
 
-    core.configurations.subscribe(function(config) {
-        $scope.$evalAsync(function() {
+    core.configurations.subscribe(function (config) {
+        $scope.$evalAsync(function () {
             $scope.config = config;
         });
     });
 
-    $scope.$on('jsonEditor.changed', function(event, json) {
+    $scope.$on('jsonEditor.changed', function (_event, json) {
         $scope.saving = true;
         core.saveConfig(json);
     });
 
-    $scope.showLocalConfig = function() {
+    $scope.showLocalConfig = function () {
         var displayConfig = angular.copy($scope.config);
         if (!$scope.includePasswords) {
-            displayConfig.forEach(function(serviceConfig) {
+            displayConfig.forEach(function (serviceConfig) {
                 delete serviceConfig.username;
                 delete serviceConfig.password;
+                delete serviceConfig.token;
             });
         }
         $scope.displayConfig = displayConfig;
     };
 
-    $scope.showFromUrl = function(url) {
+    $scope.showFromUrl = function (url) {
         $http({
-          method: 'GET',
-          url: url
-        }).then(function successCallback(response) {
-            $scope.displayConfig = response.data;
-            $scope.urlError = null;
-        }, function errorCallback(response = {}) {
-            $scope.urlError = response.statusText || 'Request failed. Status = ' + response.status;
-        });
+            method: 'GET',
+            url: url,
+        }).then(
+            function successCallback(response) {
+                $scope.displayConfig = response.data;
+                $scope.urlError = null;
+            },
+            function errorCallback(response = {}) {
+                $scope.urlError =
+                    response.statusText || `Request failed. Status = ${response.status}`;
+            },
+        );
     };
 });
