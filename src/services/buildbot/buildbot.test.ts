cbcd78f107adcc39b657cb1f04e1abaec81a4ed9
Adam Nowotny <adam.nowotny@protonmail.com>
2024-07-29 16:41:54 +0200

    BuildBot rewrite to promises

---
+++ src/services/buildbot/buildbot.test.ts
@@ -0,0 +1,98 @@
+import request from 'service-worker/request';
+import { CIBuildChange, CIServiceSettings } from 'services/service-types';
+import { beforeEach, describe, expect, it, Mock, vi } from 'vitest';
+import buildbot from './buildbot';
+import buildersJson from './builders.json';
+import lastBuildJson from './builds_complete.json';
+
+vi.mock('common/logger');
+vi.mock('service-worker/request');
+
+let settings: CIServiceSettings;
+
+beforeEach(() => {
+    settings = {
+        name: 'mock',
+        baseUrl: 'baseUrl',
+        projects: [],
+        username: 'USERNAME',
+        password: 'PASSWORD',
+        url: 'https://example.com/',
+    };
+});
+
+describe('getPipelines', () => {
+    it('passes parameters to request', async () => {
+        (request.get as Mock).mockResolvedValue({ body: buildersJson });
+
+        await buildbot.getPipelines(settings);
+
+        expect(request.get).toHaveBeenCalledWith(
+            expect.objectContaining({
+                url: 'https://example.com/api/v2/builders',
+                username: 'USERNAME',
+                password: 'PASSWORD',
+                type: 'json',
+            }),
+        );
+    });
+
+    it('parses builders', async () => {
+        (request.get as Mock).mockResolvedValue({ body: buildersJson });
+
+        const response = await buildbot.getPipelines(settings);
+
+        expect(response).toHaveLength(207);
+        expect(response).toEqual(
+            expect.arrayContaining([
+                {
+                    id: 'Apple-Sonoma-Smart-Pointer-Static-Analyzer-Build',
+                    name: 'Apple-Sonoma-Smart-Pointer-Static-Analyzer-Build',
+                },
+                {
+                    id: 'Apple-iOS-14-Simulator-Debug-Build',
+                    name: 'Apple-iOS-14-Simulator-Debug-Build',
+                },
+            ]),
+        );
+    });
+});
+
+describe('getBuildStates', () => {
+    it('passes parameters to request', async () => {
+        (request.get as Mock).mockResolvedValue({ body: lastBuildJson });
+        settings.projects = ['Apple-iOS-14-Simulator-Debug-Build'];
+
+        await buildbot.getBuildStates(settings);
+
+        expect(request.get).toHaveBeenCalledWith(
+            expect.objectContaining({
+                url: 'https://example.com/api/v2/builders/Apple-iOS-14-Simulator-Debug-Build/builds',
+                username: settings.username,
+                password: settings.password,
+                type: 'json',
+                query: {
+                    limit: 1,
+                    order: '-number',
+                },
+            }),
+        );
+    });
+
+    it('parses builds', async () => {
+        (request.get as Mock).mockResolvedValue({ body: lastBuildJson });
+        settings.projects = ['Apple-iOS-14-Simulator-Debug-Build'];
+
+        const response = await buildbot.getBuildStates(settings);
+
+        expect(response).toEqual([
+            {
+                id: 'Apple-iOS-14-Simulator-Debug-Build',
+                isBroken: true,
+                isRunning: false,
+                name: 'Apple-iOS-14-Simulator-Debug-Build',
+                webUrl: 'https://example.com/#/builders/Apple-iOS-14-Simulator-Debug-Build/builds/16837',
+            },
+        ]);
+    });
+});
--- src/services/buildbot/buildbot.ts
+++ src/services/buildbot/buildbot.ts
@@ -1,6 +1,54 @@
+import logger from 'common/logger';
 import Rx from 'rx';
-import requests from 'services/buildbot/buildbotRequests';
+import request from 'service-worker/request';
 import type { CIBuild, CIPipeline, CIServiceSettings } from 'services/service-types';
+
+const requestBuilders = (settings: CIServiceSettings) =>
+    request.get({
+        url: new URL('api/v2/builders', settings.url).href,
+        type: 'json',
+        username: settings.username,
+        password: settings.password,
+    });
+
+const requestLastBuild = (id: string, settings: CIServiceSettings) =>
+    request.get({
+        url: new URL(`api/v2/builders/${id}/builds`, settings.url).href,
+        query: { limit: 1, order: '-number' },
+        type: 'json',
+        username: settings.username,
+        password: settings.password,
+    });
+
+const getPipelines = async (settings: CIServiceSettings): Promise<CIPipeline[]> => {
+    logger.log('buildbot.getPipelines', settings);
+    const response = await requestBuilders(settings);
+    const { builders } = response.body;
+    const pipelines: CIPipeline[] = builders.map(builder => ({
+        id: builder.name,
+        name: builder.name,
+    }));
+    return pipelines;
+};
+
+const getBuildStates = async (settings: CIServiceSettings): Promise<CIBuild[]> => {
+    logger.log('buildbot.getBuildStates', settings);
+    return Promise.all(
+        settings.projects.map(async id => {
+            try {
+                const response = await requestLastBuild(id, settings);
+                const [build] = response.body.builds;
+                return parseBuild(id, settings, build);
+            } catch (ex: any) {
+                return {
+                    id: id,
+                    name: id,
+                    error: { name: 'Error', message: ex.message },
+                };
+            }
+        }),
+    );
+};
 
 export default {
     getInfo: () => ({
@@ -22,44 +70,32 @@ export default {
             password: '',
         },
     }),
-    getAll: settings =>
-        requests
-            .allBuilds(settings)
-            .select(parseAvailableBuilds)
-            .selectMany(projects => Rx.Observable.fromArray(projects)),
-    getLatest: (settings: CIServiceSettings) =>
-        Rx.Observable.fromArray(settings.projects).selectMany(id =>
-            requests
-                .lastBuild(id, settings)
-                .zip(requests.lastCompletedBuild(id, settings), (lastBuild, lastCompletedBuild) =>
-                    parseBuild(id, settings, lastBuild, lastCompletedBuild),
-                )
-                .catch(ex =>
-                    Rx.Observable.return<CIBuild>({
-                        id,
-                        name: id,
-                        error: { name: 'Error', message: ex.message },
-                    }),
-                ),
+    getAll: (settings: CIServiceSettings): Rx.Observable<CIPipeline> =>
+        Rx.Observable.fromPromise(getPipelines(settings)).flatMap(pipelines =>
+            Rx.Observable.fromArray(pipelines),
         ),
+    getLatest: (settings: CIServiceSettings): Rx.Observable<CIBuild> =>
+        Rx.Observable.fromPromise(getBuildStates(settings)).flatMap(buildStates =>
+            Rx.Observable.fromArray(buildStates),
+        ),
+    getPipelines,
+    getBuildStates,
 };
 
-function parseAvailableBuilds(apiJson: any): CIPipeline[] {
-    return Object.keys(apiJson).map(id => ({
-        id,
-        name: id,
-        group: apiJson[id].category,
-        isDisabled: false,
-    }));
+enum BuildResultCodes {
+    SUCCESS = 0,
+    WARNINGS = 1,
+    FAILURE = 2,
+    SKIPPED = 3,
+    EXCEPTION = 4,
+    RETRY = 5,
+    CANCELLED = 6,
 }
 
-const parseBuild = (id, settings, lastBuild, lastCompletedBuild): CIBuild => ({
+const parseBuild = (id, settings, build): CIBuild => ({
     id,
     name: id,
-    group: lastBuild.category,
-    webUrl: new URL(`builders/${id}`, settings.url).href,
-    isBroken: lastCompletedBuild.text.includes('failed'),
-    isRunning: lastBuild.state === 'building',
-    isDisabled: lastBuild.state === 'offline',
-    changes: lastCompletedBuild.blame.map(item => ({ name: item })),
+    webUrl: new URL(`/#/builders/${id}/builds/${build.number}`, settings.url).href,
+    isBroken: build.results !== BuildResultCodes.SUCCESS,
+    isRunning: !build.complete,
 });
