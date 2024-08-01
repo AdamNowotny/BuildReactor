b20e3a9f6a59a6eda441bccadd78410ea124b951
Adam Nowotny <adam.nowotny@protonmail.com>
2024-08-01 19:37:24 +0200

    fix: on/off switch

--- src/settings/directives/onOffSwitch/onOffSwitch.js
+++ src/settings/directives/onOffSwitch/onOffSwitch.js
@@ -2,17 +2,17 @@ import app from 'settings/app';
 import template from 'settings/directives/onOffSwitch/onOffSwitch.html';
 
 export default app.directive('onOffSwitch', () => ({
-	scope: {
-		onOff: '=onOff'
-	},
-	templateUrl: template,
-	controller($scope, $element, $attrs, $transclude) {
-		$scope.$watch('onOff', (onOff) => {
-			$scope.switch = onOff ? 'on' : 'off';
-		});
+    scope: {
+        onOff: '=onOff',
+    },
+    templateUrl: template,
+    controller($scope, $element, $attrs, $transclude) {
+        $scope.$watch('onOff', onOff => {
+            $scope.switch = onOff ? 'on' : 'off';
+        });
 
-		$scope.userSwitch = function(newValue) {
-			$scope.$emit('onOffSwitch.change', newValue === 'on');
-		};
-	}
+        $scope.userSwitch = function (newValue) {
+            $scope.$emit('onOffSwitch.change', newValue === 'on');
+        };
+    },
 }));
