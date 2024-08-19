7a4454a8e6ff2dbbbcbe8f1d100633a4f4491993
Adam Nowotny <adam.nowotny@protonmail.com>
2024-08-19 17:32:08 +0200

    popup: add tooltips using react-bootstrap

--- src/dashboard/components/navbar/navbar.tsx
+++ src/dashboard/components/navbar/navbar.tsx
@@ -1,30 +1,27 @@
 import React from 'react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import './navbar.css';
 
 export default ({ dark = false }) => {
     const navBarClass = dark ? 'navbar-inverse' : 'navbar-default';
+    const settingsTooltip = <Tooltip id="settings-tooltip">Settings</Tooltip>;
+    const dashboardTooltip = <Tooltip id="dashboard-tooltip">Dashboard</Tooltip>;
     return (
         <div className={`navbar navbar-fixed-top ${navBarClass}`} role="navigation">
             <ul className="nav navbar-nav navbar-right">
                 <li>
-                    <a
-                        href="../../settings.html"
-                        target="_blank"
-                        uib-tooltip="Settings"
-                        tooltip-placement="bottom"
-                    >
-                        <i className="fa fa-gear fa-2x"></i>
-                    </a>
+                    <OverlayTrigger placement="bottom" overlay={settingsTooltip}>
+                        <a href="../../settings.html" target="_blank">
+                            <i className="fa fa-gear fa-2x"></i>
+                        </a>
+                    </OverlayTrigger>
                 </li>
                 <li>
-                    <a
-                        href="dashboard.html"
-                        target="_blank"
-                        uib-tooltip="Dashboard"
-                        tooltip-placement="bottom"
-                    >
-                        <i className="fa fa-tasks fa-2x"></i>
-                    </a>
+                    <OverlayTrigger placement="bottom" overlay={dashboardTooltip}>
+                        <a href="dashboard.html" target="_blank">
+                            <i className="fa fa-tasks fa-2x"></i>
+                        </a>
+                    </OverlayTrigger>
                 </li>
             </ul>
         </div>
