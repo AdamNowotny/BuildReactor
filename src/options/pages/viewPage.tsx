57418284bf3a0820e8527d42c2b63e1437d32514
Adam Nowotny <adam.nowotny@protonmail.com>
2024-08-27 22:29:54 +0200

    view page styling

--- src/options/pages/viewPage.tsx
+++ src/options/pages/viewPage.tsx
@@ -7,7 +7,7 @@ import {
 } from 'components/formFields';
 import { ViewConfigContext } from 'components/react-types';
 import React, { useContext } from 'react';
-import { Col, Form } from 'react-bootstrap';
+import { Col, Container, Form, Row } from 'react-bootstrap';
 
 export default () => {
     const viewConfig = useContext(ViewConfigContext);
@@ -17,59 +17,61 @@ export default () => {
     };
 
     return (
-        <Col md={12}>
-            <Col md={4}>
-                <Form horizontal>
-                    <FormSelectField
-                        label={'Theme'}
-                        items={{ dark: 'Dark', light: 'Light' }}
-                        activeItem={viewConfig.theme}
-                        onSelect={value => {
-                            setField('theme', value);
-                        }}
-                    />
-                    <FormNumberField
-                        label="Number of Columns"
-                        min={1}
-                        max={20}
-                        onChange={value => {
-                            setField('columns', value);
-                        }}
-                    />
-                    <FormBooleanField
-                        label="Column width"
-                        items={{ false: 'Fixed', true: 'Variable' }}
-                        activeItem={viewConfig.fullWidthGroups}
-                        onSelect={value => {
-                            setField('fullWidthGroups', value);
-                        }}
-                    />
-                    <FormBooleanField
-                        label="Collapse groups"
-                        activeItem={!viewConfig.singleGroupRows}
-                        onSelect={value => {
-                            setField('singleGroupRows', !value);
-                        }}
-                    />
-                    <FormBooleanField
-                        label="Show commit messages"
-                        activeItem={viewConfig.showCommits}
-                        onSelect={value => {
-                            setField('showCommits', value);
-                        }}
-                    />
-                    <FormBooleanField
-                        label="Show commit messages for green builds"
-                        activeItem={viewConfig.showCommitsWhenGreen}
-                        onSelect={value => {
-                            setField('showCommitsWhenGreen', value);
-                        }}
-                    />
-                </Form>
-            </Col>
-            <Col md={8}>
-                <DashboardTheme popup={false} />
-            </Col>
-        </Col>
+        <Container>
+            <Row>
+                <Col md={5}>
+                    <Form>
+                        <FormSelectField
+                            label={'Theme'}
+                            items={{ dark: 'Dark', light: 'Light' }}
+                            activeItem={viewConfig.theme}
+                            onSelect={value => {
+                                setField('theme', value);
+                            }}
+                        />
+                        <FormNumberField
+                            label="Number of Columns"
+                            min={1}
+                            max={20}
+                            onChange={value => {
+                                setField('columns', value);
+                            }}
+                        />
+                        <FormBooleanField
+                            label="Column width"
+                            items={{ false: 'Fixed', true: 'Variable' }}
+                            activeItem={viewConfig.fullWidthGroups}
+                            onSelect={value => {
+                                setField('fullWidthGroups', value);
+                            }}
+                        />
+                        <FormBooleanField
+                            label="Collapse groups"
+                            activeItem={!viewConfig.singleGroupRows}
+                            onSelect={value => {
+                                setField('singleGroupRows', !value);
+                            }}
+                        />
+                        <FormBooleanField
+                            label="Show commit messages"
+                            activeItem={viewConfig.showCommits}
+                            onSelect={value => {
+                                setField('showCommits', value);
+                            }}
+                        />
+                        <FormBooleanField
+                            label="Show commit messages for green builds"
+                            activeItem={viewConfig.showCommitsWhenGreen}
+                            onSelect={value => {
+                                setField('showCommitsWhenGreen', value);
+                            }}
+                        />
+                    </Form>
+                </Col>
+                <Col md={7}>
+                    <DashboardTheme popup={false} />
+                </Col>
+            </Row>
+        </Container>
     );
 };
