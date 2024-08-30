33cd02559724b7eb3e3545e7651d16298536213a
Adam Nowotny <adam.nowotny@protonmail.com>
2024-08-30 20:56:18 +0200

    hover background over selectField in forms

--- src/common/components/forms/selectField.tsx
+++ src/common/components/forms/selectField.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { FormField } from './field';
+import './selectField.css';
 
 export const FormSelectField = ({
     label,
